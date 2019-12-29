23d90b50c2dbc2a394c3b336cce008e084428d41
ibrahimk9000 <ibra1990ski@gmail.com>
2019-12-29 21:55:00 +0100

    fix

--- webstarauto/webstarauto/pipelines.py
+++ webstarauto/webstarauto/pipelines.py
@@ -6,6 +6,6 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class BigdataPipeline(object):
+class webstarautoPipeline(object):
     def process_item(self, item, spider):
         return item
--- webstarauto/webstarauto/settings.py
+++ webstarauto/webstarauto/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for bigdata project
+# Scrapy settings for webstarauto project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,14 +9,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'bigdata'
+BOT_NAME = 'webstarauto'
 
-SPIDER_MODULES = ['bigdata.spiders']
-NEWSPIDER_MODULE = 'bigdata.spiders'
+SPIDER_MODULES = ['webstarauto.spiders']
+NEWSPIDER_MODULE = 'webstarauto.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'bigdata (+http://www.yourdomain.com)'
+#USER_AGENT = 'webstarauto (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -47,13 +47,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'bigdata.middlewares.BigdataSpiderMiddleware': 543,
+#    'webstarauto.middlewares.webstarautoSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'bigdata.middlewares.BigdataDownloaderMiddleware': 543,
+#    'webstarauto.middlewares.webstarautoDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'bigdata.pipelines.BigdataPipeline': 300,
+#    'webstarauto.pipelines.webstarautoPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
